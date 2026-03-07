f02f9b064ff7a1f55fde0d5ee48b67862bbb4287
Yifan Wu <datasciencewyfan@gmail.com>
2026-03-06 22:31:59 -0500

    isaaclab eval openpi 1st attempt

--- rlinf/models/embodiment/openpi/dataconfig/__init__.py
+++ rlinf/models/embodiment/openpi/dataconfig/__init__.py
@@ -41,6 +41,9 @@ from rlinf.models.embodiment.openpi.dataconfig.franka_dataconfig import (
 from rlinf.models.embodiment.openpi.dataconfig.gsenv_dataconfig import (
     LeRobotGSEnvDataConfig,
 )
+from rlinf.models.embodiment.openpi.dataconfig.isaaclab_dataconfig import (
+    LeRobotIsaacLabDataConfig,
+)
 from rlinf.models.embodiment.openpi.dataconfig.libero_dataconfig import (
     LeRobotLiberoDataConfig,
 )
@@ -306,6 +309,36 @@ _CONFIGS = [
             action_train_with_rotation_6d=False,  # User can add extra config in custom dataset
         ),
         pytorch_weight_path="checkpoints/torch/pi0_base",
+    ),
+    TrainConfig(
+        name="pi0_isaaclab",
+        model=pi0_config.Pi0Config(action_horizon=5),
+        data=LeRobotIsaacLabDataConfig(
+            repo_id="RLinf/IsaacLab-Stack-Cube-Data",
+            base_config=DataConfig(prompt_from_task=True),
+            assets=AssetsConfig(assets_dir="checkpoints/torch/pi0_isaaclab/assets"),
+            extra_delta_transform=False,
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader(
+            "checkpoints/jax/pi0_base/params"
+        ),
+        pytorch_weight_path="checkpoints/torch/pi0_base",
+    ),
+    TrainConfig(
+        name="pi05_isaaclab",
+        model=pi0_config.Pi0Config(
+            pi05=True, action_horizon=5, discrete_state_input=False
+        ),
+        data=LeRobotIsaacLabDataConfig(
+            repo_id="RLinf/IsaacLab-Stack-Cube-Data",
+            base_config=DataConfig(prompt_from_task=True),
+            assets=AssetsConfig(assets_dir="checkpoints/torch/pi0_isaaclab/assets"),
+            extra_delta_transform=False,
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader(
+            "checkpoints/jax/pi05_base/params"
+        ),
+        pytorch_weight_path="checkpoints/torch/pi05_base",
     ),
 ]
 
