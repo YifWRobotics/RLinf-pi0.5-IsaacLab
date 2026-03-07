bb41d5dcc47d4ef0e2843286dd9d0a2aa0e6733c
Yifan Wu <datasciencewyfan@gmail.com>
2026-03-07 18:38:05 -0500

    action chunk set to be 10

--- rlinf/models/embodiment/openpi/dataconfig/__init__.py
+++ rlinf/models/embodiment/openpi/dataconfig/__init__.py
@@ -311,23 +311,9 @@ _CONFIGS = [
         pytorch_weight_path="checkpoints/torch/pi0_base",
     ),
     TrainConfig(
-        name="pi0_isaaclab",
-        model=pi0_config.Pi0Config(action_horizon=5),
-        data=LeRobotIsaacLabDataConfig(
-            repo_id="RLinf/IsaacLab-Stack-Cube-Data",
-            base_config=DataConfig(prompt_from_task=True),
-            assets=AssetsConfig(assets_dir="checkpoints/torch/pi0_isaaclab/assets"),
-            extra_delta_transform=False,
-        ),
-        weight_loader=weight_loaders.CheckpointWeightLoader(
-            "checkpoints/jax/pi0_base/params"
-        ),
-        pytorch_weight_path="checkpoints/torch/pi0_base",
-    ),
-    TrainConfig(
         name="pi05_isaaclab",
         model=pi0_config.Pi0Config(
-            pi05=True, action_horizon=5, discrete_state_input=False
+            pi05=True, action_horizon=10, discrete_state_input=False
         ),
         data=LeRobotIsaacLabDataConfig(
             repo_id="RLinf/IsaacLab-Stack-Cube-Data",
